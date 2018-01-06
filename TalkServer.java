fb535db1c1c11e3b95d0db67f4c1b3bf1cdb6266
刘欣昂 <744749175@qq.com>
2018-01-07 01:43:12 +0800

    Update TalkServer.java
    Format

--- TalkServer.java
+++ TalkServer.java
@@ -11,28 +11,29 @@ public class TalkServer {
 	}
 }
 
-class InfFrame extends Frame{
+class InfFrame extends Frame {
 	TextArea tf = new TextArea();
 	TextField tf_in = null;
 	ServerSocket ss = null;
 	Socket s = null;
 	DataOutputStream dos = null;
 	DataInputStream dis = null;
-	boolean socketconnect = false ;
-	int addressmain ;
+	boolean socketconnect = false;
+	int addressmain;
 	ArrayList<ServerStart> al = new ArrayList<ServerStart>();
+
 	InfFrame() {
 		setTitle("Server");
-		setBounds(300,300,700,300);		
-		setLayout(null);	
+		setBounds(300, 300, 700, 300);
+		setLayout(null);
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				setVisible(false);
 				try {
 					ss.close();
-				}catch(NullPointerException aep) {
+				} catch (NullPointerException aep) {
 					System.out.println("Already closed!");
-				}catch(IOException ae) {
+				} catch (IOException ae) {
 					tf.setText("Can't Close the Socket!");
 				}
 				System.exit(0);
@@ -41,47 +42,47 @@ class InfFrame extends Frame{
 		Font f = new Font(Font.DIALOG, Font.PLAIN, 28);
 		tf.setFont(f);
 		tf.setEditable(false);
-		tf.setText("Need input Address."+'\n'+'\n'+"No Connected.");
-		tf.setBounds(5,50,525,250);
+		tf.setText("Need input Address." + '\n' + '\n' + "No Connected.");
+		tf.setBounds(5, 50, 525, 250);
 		Panel p = new Panel();
-		p.setBounds(530,50,170,250);
+		p.setBounds(530, 50, 170, 250);
 		p.setLayout(null);
 		tf_in = new TextField();
-		tf_in.setBounds(0,0,100,40);
-		tf_in.setFont(new Font("宋体",48,32));
-		tf_in.addActionListener(new ActionListener(){
+		tf_in.setBounds(0, 0, 100, 40);
+		tf_in.setFont(new Font("瀹嬩綋", 48, 32));
+		tf_in.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String address = tf_in.getText();
-				int add = 0 ;
-				boolean flag = false ;
+				int add = 0;
+				boolean flag = false;
 				try {
 					add = Integer.parseInt(address);
-					flag = true ;
-				}catch(NumberFormatException ae4) {
-					tf.setText(tf.getText()+'\n'+"Address Error!");
-				}		
-				if(flag) {
-					if(add<10000&&add>1024) {
-						addressmain = add ;
-						tf.setText("Address:"+add+'\n'+'\n'+"No Connected.");
-					}else {
-						tf.setText(tf.getText()+'\n'+"Address Error!");
+					flag = true;
+				} catch (NumberFormatException ae4) {
+					tf.setText(tf.getText() + '\n' + "Address Error!");
+				}
+				if (flag) {
+					if (add < 10000 && add > 1024) {
+						addressmain = add;
+						tf.setText("Address:" + add + '\n' + '\n' + "No Connected.");
+					} else {
+						tf.setText(tf.getText() + '\n' + "Address Error!");
 					}
 				}
 			}
 		});
 		Button b = new Button("Random");
-		b.setBounds(100,0,60,40);
-		b.setBackground(new Color(193,255,193));
+		b.setBounds(100, 0, 60, 40);
+		b.setBackground(new Color(193, 255, 193));
 		b.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int address = 0 ;
+				int address = 0;
 				do {
-					address = (int)Math.rint( Math.random()*10000);
-				}while(address<1024);				
-				tf_in.setText(""+address);
-				addressmain = address ;
-				tf.setText("Address:"+address+'\n'+'\n'+"No Connected.");
+					address = (int) Math.rint(Math.random() * 10000);
+				} while (address < 1024);
+				tf_in.setText("" + address);
+				addressmain = address;
+				tf.setText("Address:" + address + '\n' + '\n' + "No Connected.");
 			}
 		});
 		Button b_sure = new Button("Sure");
@@ -92,148 +93,147 @@ class InfFrame extends Frame{
 		p.add(b_sure);
 		add(tf);
 		add(p);
-		setVisible(true);	
-//		for(;;) {			
-//			System.out.println("");	
-//			int a = 0 ;
-//			a++;		
-//			continue;
-//			}		
-		}
-	
+		setVisible(true);
+		// for(;;) {
+		// System.out.println("");
+		// int a = 0 ;
+		// a++;
+		// continue;
+		// }
+	}
+
 	public void Started(int add) throws IOException {
-//		System.out.println("!");
-//		DataOutputStream dos = null;
-//		DataInputStream dis = null;
-//		System.out.println("!");		
-		boolean bconnect = false ;
-		{		
+		// System.out.println("!");
+		// DataOutputStream dos = null;
+		// DataInputStream dis = null;
+		// System.out.println("!");
+		boolean bconnect = false;
+		{
 			ss = new ServerSocket(add);
-			socketconnect = true ;
-//			System.out.println(socketconnect);
-			bconnect = true ;
+			socketconnect = true;
+			// System.out.println(socketconnect);
+			bconnect = true;
 		}
-		
+
 		try {
-			while(bconnect) {	
+			while (bconnect) {
 				s = ss.accept();
-//				可以连接Socket										
-//				tf.setText("ID:"+add);
+				// 鍙互杩炴帴Socket
+				// tf.setText("ID:"+add);
 				ServerStart serverstart = new ServerStart(s);
 				new Thread(serverstart).start();
 				al.add(serverstart);
-				tf.setText(tf.getText()+'\n'+"Socket connected Successful!");
-			}			
-//			System.out.println("!");
-		}catch(IOException ae) {
+				tf.setText(tf.getText() + '\n' + "Socket connected Successful!");
+			}
+			// System.out.println("!");
+		} catch (IOException ae) {
 			ae.printStackTrace();
-			tf.setText("ID:8678"+'\n'+"Failed to connected.");
-		}finally {
+			tf.setText("ID:8678" + '\n' + "Failed to connected.");
+		} finally {
 			try {
 				s.close();
-			}catch (IOException ae) {
+			} catch (IOException ae) {
 				System.out.println("Error!");
 			}
 		}
-//		new Thread(new ServerStart(s)).start();
+		// new Thread(new ServerStart(s)).start();
 	}
-	
-	class ServerStart implements Runnable{
+
+	class ServerStart implements Runnable {
 		Socket s = null;
-		boolean connect = false;	
+		boolean connect = false;
 		DataInputStream dis = null;
 		DataOutputStream dos = null;
-//		String word = "";
-		ServerStart(){
-			/*Do Notihing */
+
+		// String word = "";
+		ServerStart() {
+			/* Do Notihing */
 		}
-		
-		ServerStart(Socket s){
-//			this.dis = dis ;
-//			this.dos = dos ;
-			this.s = s ;			
+
+		ServerStart(Socket s) {
+			// this.dis = dis ;
+			// this.dos = dos ;
+			this.s = s;
 			try {
 				dis = new DataInputStream(s.getInputStream());
 				dos = new DataOutputStream(s.getOutputStream());
 				connect = true;
-			}catch(NullPointerException aen) {
+			} catch (NullPointerException aen) {
 				System.out.println("NullPointerException.");
-			}catch(IOException ae3) {
+			} catch (IOException ae3) {
 				System.out.println("DataInOutputStream error.Server Close.");
 			}
 		}
-		
-		public void send (String word) {
+
+		public void send(String word) {
+			try {
+				dos.writeUTF(word);
+				// System.out.println("send");
+			} catch (IOException ae2) {
 				try {
-					dos.writeUTF(word);
-//					System.out.println("send");
-				}catch(IOException ae2) {
-					try {
-						dos.flush();
-						dos.close();
-						ss.close();
-					}catch(IOException ae3) {
-						ae3.printStackTrace();
-					}					
-				}				
+					dos.flush();
+					dos.close();
+					ss.close();
+				} catch (IOException ae3) {
+					ae3.printStackTrace();
+				}
+			}
 		}
-		
+
 		public void run() {
-			String word = "" ;
-			while(connect) {
-				try{
+			String word = "";
+			while (connect) {
+				try {
 					word = dis.readUTF();
-					for(int i = 0 ; i < al.size() ; i++) {
+					for (int i = 0; i < al.size(); i++) {
 						ServerStart sstart = al.get(i);
 						sstart.send(word);
-//						try {
-//							
-//						}catch(IOException ae2) {
-//							ae2.printStackTrace();
-//						}
+						// try {
+						//
+						// }catch(IOException ae2) {
+						// ae2.printStackTrace();
+						// }
 					}
 					System.out.println("read");
 				}
-//				catch(SocketException aesocket) {
-//					System.out.println("!");
-//				}
-				catch(IOException ae) {
-					try {						
-							dis.close();												
-							s.close();											
-							dos.close();
-							break;
-//							System.exit(0);
+				// catch(SocketException aesocket) {
+				// System.out.println("!");
+				// }
+				catch (IOException ae) {
+					try {
+						dis.close();
+						s.close();
+						dos.close();
+						break;
+						// System.exit(0);
 					} catch (IOException ae3) {
 						System.out.println("Close Failed!");
-						}
+					}
 					ae.printStackTrace();
 				}
-				
+
 			}
 		}
 	}
-	
-	class Monitor implements ActionListener{		
-	public void actionPerformed(ActionEvent e) {
-			if(addressmain>1024&&addressmain<10000) {
+
+	class Monitor implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (addressmain > 1024 && addressmain < 10000) {
 				try {
 					System.out.println("Started!");
-					System.out.println(addressmain);	
-					tf.setText(tf.getText()+'\n'+"Successful launch.");
+					System.out.println(addressmain);
+					tf.setText(tf.getText() + '\n' + "Successful launch.");
 					Started(addressmain);
-				}catch(BindException aee) {
-					tf.setText("This Address has already been used!"+'\n'+"Please close it!");
-				}catch(IOException ae) {
+				} catch (BindException aee) {
+					tf.setText("This Address has already been used!" + '\n' + "Please close it!");
+				} catch (IOException ae) {
 					ae.printStackTrace();
 					tf.setText("Failed to launch.");
 				}
-//				System.out.println(isconnect);
-			}else {
-				tf.setText(tf.getText()+'\n'+'\n'+"Address not in range.");
-			}					
+				// System.out.println(isconnect);
+			} else {
+				tf.setText(tf.getText() + '\n' + '\n' + "Address not in range.");
+			}
 		}
 	}
 }
-
-
